b8e12795023578af2ddefc5b35efcece8c436a1f
Huang Yuan haung <dha834773759@163.com>
2019-09-05 12:58:45 +0800

    用户登录接口

---
+++ Common/MMK.SmartSystem.Common/Model/RequestResult.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMK.SmartSystem.Common.Model
+{
+    public class RequestResult<T> where T : class
+    {
+        [JsonProperty("result", Required = Required.Default, NullValueHandling = NullValueHandling.Ignore)]
+
+        public T Result { get; set; }
+
+        [JsonProperty("targetUrl", Required = Required.Default, NullValueHandling = NullValueHandling.Ignore)]
+        public string TargetUrl { get; set; }
+
+        [JsonProperty("success", Required = Required.Default, NullValueHandling = NullValueHandling.Ignore)]
+
+        public bool Success { get; set; }
+        [JsonProperty("error", Required = Required.Default, NullValueHandling = NullValueHandling.Ignore)]
+
+        public RequestError Error { get; set; }
+        [JsonProperty("unAuthorizedRequest", Required = Required.Default, NullValueHandling = NullValueHandling.Ignore)]
+
+        public bool UnAuthorizedRequest { get; set; }
+    }
+
+
+    public class RequestError
+    {
+        [JsonProperty("code", Required = Required.Default, NullValueHandling = NullValueHandling.Ignore)]
+
+        public int Code { get; set; }
+
+        [JsonProperty("message", Required = Required.Default, NullValueHandling = NullValueHandling.Ignore)]
+
+        public string Message { get; set; }
+
+        [JsonProperty("details", Required = Required.Default, NullValueHandling = NullValueHandling.Ignore)]
+
+        public string Details { get; set; }
+        [JsonProperty("validationErrors", Required = Required.Default, NullValueHandling = NullValueHandling.Ignore)]
+
+        public string ValidationErrors { get; set; }
+    }
+}
