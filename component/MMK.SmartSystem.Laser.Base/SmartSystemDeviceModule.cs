fed1fb6cda5924c403e6056588f4700370182b63
jinji.wu <aoe123jiji@163.com>
2019-09-06 09:06:34 +0800

    翻译

--- component/MMK.SmartSystem.Laser.Base/SmartSystemDeviceModule.cs
+++ component/MMK.SmartSystem.Laser.Base/SmartSystemDeviceModule.cs
@@ -16,19 +16,19 @@ namespace MMK.SmartSystem.Laser.Base
         {
             List<SystemMenuModule> menuModules = new List<SystemMenuModule>{
                 new SystemMenuModule{
-                    ModuleName = "机床功能",
+                    ModuleName = "MachineOperation",
                     Pages = new List<SystemPageModel>{
-                        new SystemPageModel(){ Title="自动寻边",FullName="MMK.SmartSystem.Laser.Base.MachineOperation.AutoFindSidePage",IsAuth=true,Permission="MachineOperation.AutoFindSidePage"},
-                        new SystemPageModel(){ Title="辅助气体",FullName="MMK.SmartSystem.Laser.Base.MachineOperation.AuxGasCheckPage",IsAuth=true,Permission="MachineOperation.AuxGasCheckPage"},
-                        new SystemPageModel(){ Title="手动寻边",FullName="MMK.SmartSystem.Laser.Base.MachineOperation.ManualFindSidePage",IsAuth=true,Permission="MachineOperation.ManualFindSidePage"},
+                        new SystemPageModel(){ Title="MachineOperation.AutoFindSide",FullName="MMK.SmartSystem.Laser.Base.MachineOperation.AutoFindSidePage",IsAuth=true,Permission="MachineOperation.AutoFindSidePage"},
+                        new SystemPageModel(){ Title="MachineOperation.AuxGasCheck",FullName="MMK.SmartSystem.Laser.Base.MachineOperation.AuxGasCheckPage",IsAuth=true,Permission="MachineOperation.AuxGasCheckPage"},
+                        new SystemPageModel(){ Title="MachineOperation.ManualFindSide",FullName="MMK.SmartSystem.Laser.Base.MachineOperation.ManualFindSidePage",IsAuth=true,Permission="MachineOperation.ManualFindSidePage"},
                     }
                 },
                 new SystemMenuModule{
-                     ModuleName = "割嘴功能",
+                     ModuleName = "ManualFindSide",
                      Pages = new List<SystemPageModel>{
-                        new SystemPageModel(){ Title="割嘴复归",FullName="MMK.SmartSystem.Laser.Base.MachineOperation.CutterResetCheckPage",IsAuth=true,Permission="MachineOperation.CutterResetCheckPage"},
-                        new SystemPageModel(){ Title="割嘴清洁",FullName="MMK.SmartSystem.Laser.Base.MachineOperation.AutoCutterCleanPage",IsAuth=true,Permission="MachineOperation.AutoCutterCleanPage"},
-                        new SystemPageModel(){ Title="割嘴对中",FullName="MMK.SmartSystem.Laser.Base.MachineOperation.CutCenterPage",IsAuth=true,Permission="MachineOperation.CutCenterPage"}
+                        new SystemPageModel(){ Title="ManualFindSide.CutterResetCheck",FullName="MMK.SmartSystem.Laser.Base.MachineOperation.CutterResetCheckPage",IsAuth=true,Permission="MachineOperation.CutterResetCheckPage"},
+                        new SystemPageModel(){ Title="ManualFindSide.AutoCutterClean",FullName="MMK.SmartSystem.Laser.Base.MachineOperation.AutoCutterCleanPage",IsAuth=true,Permission="MachineOperation.AutoCutterCleanPage"},
+                        new SystemPageModel(){ Title="ManualFindSide.CutCenter",FullName="MMK.SmartSystem.Laser.Base.MachineOperation.CutCenterPage",IsAuth=true,Permission="MachineOperation.CutCenterPage"}
                      }
                 }
             };
