a9a8786b61ee2e27048fe8a0c9d72ccd04f1ca7f
Huang Yuan haung <dha834773759@163.com>
2019-09-16 12:14:48 +0800

    pageenum

---
+++ client/MMK.SmartSystem.LE.Host/SystemControl/ViewModel/PageChangeModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMK.SmartSystem.LE.Host.SystemControl.ViewModel
+{
+    public enum PageEnum
+    { 
+        WPFPage,
+        WebPage
+    }
+    public class PageChangeModel
+    {
+        public Type FullType { get; set; }
+
+        public PageEnum Page { get; set; }
+
+    }
+}
