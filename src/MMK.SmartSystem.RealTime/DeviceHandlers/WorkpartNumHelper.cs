4de9ed103c7e7beffcb79796b938a1ecbd505ba8
L Shao <491429058@qq.com>
2019-09-25 13:07:03 +0800

    cnc workpart

---
+++ src/MMK.SmartSystem.RealTime/DeviceHandlers/WorkpartNumHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMK.SmartSystem.RealTime.DeviceHandlers
+{
+    public static class WorkpartNumHelper
+    {
+        //public static Tuple<short, string> ReadWorkpartNum(ushort flib, ref read data)
+        //{
+
+        //}
+    }
+}
