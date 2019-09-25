06151d83ebcd38cdd43885e54e12100138e49b1a
L Shao <491429058@qq.com>
2019-09-25 14:24:13 +0800

    激光库ini

---
+++ module/MMK.CNC.Core/LaserLibrary/Gas.cs
@@ -0,0 +1,42 @@
+﻿using Abp.Domain.Entities;
+using Abp.Domain.Entities.Auditing;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace MMK.CNC.Core.LaserLibrary
+{
+    [Table("LaserLibrary_Gas")]
+    public class Gas : Entity<string>, IHasCreationTime
+    {
+        /// <summary>
+        /// 气体编号
+        /// </summary>
+        public int Code { get; set; }
+
+        /// <summary>
+        /// 气体名称（英文）
+        /// </summary>
+        [StringLength(100)]
+        public string Name_EN { get; set; }
+
+        /// <summary>
+        /// 气体名称（中文）
+        /// </summary>
+        [StringLength(100)]
+        public string Name_CN { get; set; }
+
+        /// <summary>
+        /// 备注信息
+        /// </summary>
+        public string Description { get; set; }
+
+        public DateTime CreationTime { get; set; }
+        public Gas()
+        {
+            CreationTime = DateTime.Now;
+        }
+    }
+}
