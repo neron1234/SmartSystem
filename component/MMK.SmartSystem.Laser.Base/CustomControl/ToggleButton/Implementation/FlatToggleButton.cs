69ebb8a92ce229deda38c9b2e42428644911418e
jinji.wu <aoe123jiji@163.com>
2019-09-25 16:48:06 +0800

    ToggleBtn

---
+++ component/MMK.SmartSystem.Laser.Base/CustomControl/ToggleButton/Implementation/FlatToggleButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls.Primitives;
+
+namespace MMK.SmartSystem.Laser.Base.CustomControl
+{
+    public class FlatToggleButton : ToggleButton
+    {
+        static FlatToggleButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FlatToggleButton), new FrameworkPropertyMetadata(typeof(FlatToggleButton)));
+        }
+    }
+}
