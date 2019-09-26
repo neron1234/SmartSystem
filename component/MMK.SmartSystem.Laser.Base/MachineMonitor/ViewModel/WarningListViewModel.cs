c76eabe22a23236378cb168592e832236dd47465
jinji.wu <aoe123jiji@163.com>
2019-09-26 08:29:56 +0800

    WaringMessageList

---
+++ component/MMK.SmartSystem.Laser.Base/MachineMonitor/ViewModel/WarningListViewModel.cs
@@ -0,0 +1,58 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMK.SmartSystem.Laser.Base.MachineMonitor.ViewModel
+{
+    public class WarningListViewModel:ViewModelBase
+    {
+        private ObservableCollection<WarningInfo> _WarningList;
+        public ObservableCollection<WarningInfo> WarningList
+        {
+            get { return _WarningList; }
+            set {
+                if (_WarningList != value){
+                    _WarningList = value;
+                    RaisePropertyChanged(() => WarningList);
+                }
+            }
+        }
+        public WarningListViewModel()
+        {
+            WarningList = new ObservableCollection<WarningInfo>() {
+                new WarningInfo {
+                    Id = "15",
+                    Number = "1001",
+                    Message = "THIS IS A ALARM NO 1 CURRENT"
+                },new WarningInfo {
+                    Id = "15",
+                    Number = "1002",
+                    Message = "THIS IS A ALARM NO 2 CURRENT"
+                },new WarningInfo {
+                    Id = "15",
+                    Number = "1003",
+                    Message = "THIS IS A ALARM NO 3 CURRENT"
+                },new WarningInfo {
+                    Id = "16",
+                    Number = "1004",
+                    Message = "THIS IS A ALARM NO 4 CURRENT"
+                },new WarningInfo {
+                    Id = "16",
+                    Number = "1005",
+                    Message = "THIS IS A ALARM NO 5 CURRENT"
+                }
+            };
+        }
+    }
+
+    public class WarningInfo
+    {
+        public string Id { get; set; }
+        public string Number { get; set; }
+        public string Message { get; set; }
+    }
+}
