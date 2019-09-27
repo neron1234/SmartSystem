bb24a47686827e2a98025f4659d4598bed6dc2a7
jinji.wu <aoe123jiji@163.com>
2019-09-27 13:40:57 +0800

    ToolTip

--- client/MMK.SmartSystem.LE.Host/SystemControl/HeaderMenuControl.xaml.cs
+++ client/MMK.SmartSystem.LE.Host/SystemControl/HeaderMenuControl.xaml.cs
@@ -5,6 +5,7 @@ using MMK.SmartSystem.Common;
 using MMK.SmartSystem.Common.EventDatas;
 using MMK.SmartSystem.Common.Model;
 using MMK.SmartSystem.LE.Host.AccountControl;
+using MMK.SmartSystem.LE.Host.CustomControl;
 using MMK.SmartSystem.LE.Host.SystemControl.ViewModel;
 using MMK.SmartSystem.LE.Host.ViewModel;
 using System;
@@ -75,12 +76,19 @@ namespace MMK.SmartSystem.LE.Host.SystemControl
             Messenger.Default.Send((UserControl)new PopupWindowControl(new FunctionConfigControl(),900,600));
         }
 
+        Notifiaction notifiaction = new Notifiaction();
         private void FunctionConfig2Btn_Click(object sender, RoutedEventArgs e)
         {
-            Messenger.Default.Send(new BottomWarningLogViewModel()
+            //Messenger.Default.Send(new BottomWarningLogViewModel()
+            //{
+            //    WarningLogStr = "测试出现了BUG！------" + DateTime.Now
+            //}); 
+            notifiaction.AddNotifiaction(new NotifiactionModel()
             {
-                WarningLogStr = "测试出现了BUG！------" + DateTime.Now
-            }); 
+                Title = "这是Error通知标题",
+                Content = "这条通知不会自动关闭，需要点击关闭按钮",
+                NotifiactionType = EnumPromptType.Error
+            });
         }
     }
 }
