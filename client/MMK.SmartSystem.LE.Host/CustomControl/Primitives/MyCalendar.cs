b69626abed7c55fceffd9522f3cb4800b6dec772
jinji.wu <aoe123jiji@163.com>
2019-09-27 15:56:38 +0800

    TextBox Style

---
+++ client/MMK.SmartSystem.LE.Host/CustomControl/Primitives/MyCalendar.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MMK.SmartSystem.LE.Host.CustomControl
+{
+    public class MyCalendar : Calendar
+    {
+        static MyCalendar()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(MyCalendar), new FrameworkPropertyMetadata(typeof(MyCalendar)));
+        }
+    }
+}
