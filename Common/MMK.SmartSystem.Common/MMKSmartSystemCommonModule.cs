8437317f942a242c19a20d00c8dd0eb5d0e97340
Huang Yuan haung <dha834773759@163.com>
2019-10-08 14:00:59 +0800

    自动加载配置

--- Common/MMK.SmartSystem.Common/MMKSmartSystemCommonModule.cs
+++ Common/MMK.SmartSystem.Common/MMKSmartSystemCommonModule.cs
@@ -1,8 +1,12 @@
-﻿using Abp.Modules;
+﻿using Abp.Configuration.Startup;
+using Abp.Modules;
 using Abp.Reflection.Extensions;
+using MMK.SmartSystem.Common.Model;
 using MMK.SmartSystem.WebCommon;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -15,12 +19,61 @@ namespace MMK.SmartSystem.Common
     {
         public override void PreInitialize()
         {
-            
+
         }
         public override void Initialize()
         {
             IocManager.RegisterAssemblyByConvention(Assembly.GetExecutingAssembly());
         }
 
+
+        public static void LoadJsonConfig(Type type, IAbpStartupConfiguration abpStartup)
+        {
+
+            string path = type.GetAssembly().GetDirectoryPathOrNull();
+            string fileName = type.GetAssembly().GetName().Name;
+            string fullName = Path.Combine(path, fileName + ".json");
+            if (File.Exists(fullName))
+            {
+                var list = JsonConvert.DeserializeObject<List<SystemMenuModule>>(File.ReadAllText(fullName));
+                abpStartup.GetOrCreate(SmartSystemCommonConsts.ModulePageKey, () => new List<SystemMenuModule>()).AddRange(list);
+
+            }
+            string jsonConfig = typeof(MMKSmartSystemCommonModule).GetAssembly().GetDirectoryPathOrNull();
+            List<string> jsonList = new List<string>();
+            List<SignalrQueryParmModel> signalrQueryParmModels = new List<SignalrQueryParmModel>();
+            GetAllFile(Path.Combine(jsonConfig, "Configs"), jsonList);
+
+            foreach (var item in jsonList)
+            {
+                string data = File.ReadAllText(item);
+                var arr = Path.GetDirectoryName(item).Split('\\');
+                string pathModule = arr[arr.Length - 1];
+
+                var pageNode = JsonConvert.DeserializeObject<SignlarPageParm>(data);
+                var moduleParm = signalrQueryParmModels.FirstOrDefault(d => d.Module == pathModule);
+                if (moduleParm == null)
+                {
+                    moduleParm = new SignalrQueryParmModel()
+                    {
+                        Module = pathModule,
+                        Pages = new List<SignlarPageParm>()
+                    };
+                }
+                moduleParm.Pages.Add(pageNode);
+            }
+            abpStartup.GetOrCreate(SmartSystemCommonConsts.ModuleQueryParmKey, () => new List<SignalrQueryParmModel>()).AddRange(signalrQueryParmModels);
+
+        }
+
+        private static void GetAllFile(string path, List<string> jsonList)
+        {
+            var paths = new DirectoryInfo(path).GetDirectories();
+            jsonList.AddRange(Directory.GetFiles(path, "*.json"));
+            foreach (var item in paths)
+            {
+                GetAllFile(item.FullName, jsonList);
+            }
+        }
     }
 }
