f7cdf7773f376eaed764683216ed7753c37ec995
jinji.wu <aoe123jiji@163.com>
2019-10-09 13:30:48 +0800

    Page

---
+++ component/MMK.SmartSystem.Laser.Base/MachineProcess/UserControls/ProcessOptionsControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using MMK.SmartSystem.Laser.Base.MachineProcess.UserControls.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MMK.SmartSystem.Laser.Base.MachineProcess.UserControls
+{
+    /// <summary>
+    /// ProcessOptionsControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class ProcessOptionsControl : UserControl
+    {
+        private ProcessOptionsViewModel processOptionsViewModel { get; set; }
+        public ProcessOptionsControl()
+        {
+            InitializeComponent();
+            this.DataContext = processOptionsViewModel = new ProcessOptionsViewModel();
+        }
+    }
+}
