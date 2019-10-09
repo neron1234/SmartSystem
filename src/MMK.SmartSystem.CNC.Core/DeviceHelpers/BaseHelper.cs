dfc0135a7ef4dbe7c24abc35c35194b9033971bf
L Shao <491429058@qq.com>
2019-10-09 10:03:44 +0800

    程序传输cnc接口

--- src/MMK.SmartSystem.CNC.Core/DeviceHelpers/BaseHelper.cs
+++ src/MMK.SmartSystem.CNC.Core/DeviceHelpers/BaseHelper.cs
@@ -36,5 +36,91 @@ namespace MMK.SmartSystem.CNC.Core.DeviceHelpers
             flib = 0;
         }
 
+        protected static string GetGeneralErrorMessage(short ret)
+        {
+            string res = null;
+
+            switch (ret)
+            {
+                case -16:
+                    res = "连接失败";
+                    break;
+                case -15:
+                    res = "NO DLL";
+                    break;
+                case -11:
+                    res = "BUS ERROR";
+                    break;
+                case -7:
+                    res = "系统版本不匹配";
+                    break;
+                case -6:
+                    res = "发生意外错误";
+                    break;
+                case -2:
+                    res = "操作期间执行了复位或者停止";
+                    break;
+                case -1:
+                    res = "系统忙碌";
+                    break;
+                case 1:
+                    res = "功能不可用";
+                    break;
+                case 2:
+                    res = "数据长度错误或者数据值错误";
+                    break;
+                case 3:
+                    res = "数据NUMBER错误";
+                    break;
+                case 4:
+                    res = "数据属性错误";
+                    break;
+                case 5:
+                    res = "数据错误";
+                    break;
+                case 6:
+                    res = "没有选项功能";
+                    break;
+                case 7:
+                    res = "写保护";
+                    break;
+                case 8:
+                    res = "CNC内存溢出";
+                    break;
+                case 9:
+                    res = "参数设定错误";
+                    break;
+                case 10:
+                    res = "内存为空或者内存已满";
+                    break;
+                case 11:
+                    res = "路径设定错误";
+                    break;
+                case 12:
+                    res = "CNC操作方式错误,或者CNC运行中";
+                    break;
+                case 13:
+                    res = "CNC拒绝，请检查相关状态";
+                    break;
+                case 14:
+                    res = "数据服务器错误";
+                    break;
+                case 15:
+                    res = "请消除相关报警";
+                    break;
+                case 16:
+                    res = "CNC停止或者急停";
+                    break;
+                case 17:
+                    res = "数据保护";
+                    break;
+                default:
+                    res = " 其他错误";
+                    break;
+            }
+
+            return res;
+        }
+
     }
 }
---
+++ src/MMK.SmartSystem.CNC.Core/DeviceHelpers/ProgramTransferHelper.cs
@@ -0,0 +1,173 @@
+﻿using MMK.SmartSystem.WebCommon.DeviceModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace MMK.SmartSystem.CNC.Core.DeviceHelpers
+{
+    public class ProgramTransferHelper : BaseHelper
+    {
+        public event Action<double> ReportProcessEvent;
+
+        public string LocalDownloadProgramFromPcToCnc(string pcPath, string ncFolder)
+        {
+            ushort flib = 0;
+            var ret_conn = BuildConnect(ref flib);
+            if (ret_conn != 0)
+            {
+                FreeConnect(flib);
+                return "传输程序至CNC失败，连接错误";
+            }
+
+            string progStr = "";
+            using (System.IO.StreamReader sr = new System.IO.StreamReader(pcPath))
+            {
+                progStr = sr.ReadToEnd();
+            }
+            progStr = progStr.Replace("\r\n", "\n");
+
+            var ret = Focas1.cnc_dwnstart4(flib, 0, ncFolder);//开始传输
+            if (ret != 0) 
+            {
+                FreeConnect(flib);
+                return "传输程序至CNC失败," + GetGeneralErrorMessage(ret);
+            }
+
+            var total_size = progStr.Length;
+            var len = total_size;
+            int n = 0;
+            long cur_size = 0;
+
+            while (len > 0)
+            {
+                n = len;
+                ret = Focas1.cnc_download4(flib, ref n, progStr);
+                if (ret == 10)
+                {
+                    continue;
+                }
+                if (ret == 0)
+                {
+                    cur_size += n;
+                    progStr = progStr.Substring(n);
+                    len -= n;
+                }
+                if (ret != 0)
+                {
+                    break;
+                }
+
+                if(total_size!=0)
+                {
+                    ReportProcessEvent?.Invoke((double)cur_size / (double)total_size);
+                }
+            }
+
+            Focas1.cnc_dwnend4(flib);
+
+            FreeConnect(flib);
+            if (ret != 0)
+            {
+                return "传输程序至CNC失败," + GetGeneralErrorMessage(ret);
+            }
+
+            return null;
+        }
+
+        public string LocalUploadProgramFromCncToPc(string ncPath, string pcPath, double fileSize)
+        {
+            if (System.IO.File.Exists(pcPath))
+            {
+                return "传输程序至PC失败,PC文件已经存在";
+            }
+
+            ushort flib = 0;
+            var ret_conn = BuildConnect(ref flib);
+            if (ret_conn != 0)
+            {
+                FreeConnect(flib);
+                return "传输程序至PC失败，连接错误";
+            }
+
+            string str = "";
+            long cur_size = 0;
+
+            Focas1.cnc_upend4(flib);
+            var ret = Focas1.cnc_upstart4(flib, 0, ncPath);
+            if (ret != 0)
+            {
+                FreeConnect(flib);
+                return "传输程序至PC失败," + GetGeneralErrorMessage(ret);
+            }
+
+            int len;
+            do
+            {
+                len = 1024;
+
+                StringBuilder buf = new StringBuilder(1024);
+                ret = Focas1.cnc_upload4(flib, ref len, buf);
+
+                if (ret == 10)
+                {
+                    continue;
+                }
+                if (ret == 0)
+                {
+                    string temp = buf.ToString(0, len);
+                    cur_size += len;
+                    str += temp;
+
+                }
+                if (buf[len - 1] == '%')
+                {
+                    break;
+                }
+
+                if (fileSize != 0)
+                {
+                    ReportProcessEvent?.Invoke((double)cur_size / (double)fileSize);
+                }
+
+            } while ((ret == 0) || (ret == 10));
+
+            Focas1.cnc_upend4(flib);
+
+            if (ret != 0)
+            {
+                return "传输程序至PC失败," + GetGeneralErrorMessage(ret);
+            }
+            FreeConnect(flib);
+
+            using (StreamWriter sw = new StreamWriter(pcPath))
+            {
+                sw.Write(str);
+            }
+
+            return null;
+        }
+
+        public string DeleteProgramInCnc(string ncPath)
+        {
+            ushort flib = 0;
+            var ret_conn = BuildConnect(ref flib);
+            if (ret_conn != 0)
+            {
+                FreeConnect(flib);
+                return "删除PC程序失败，连接错误";
+            }
+
+            var ret = Focas1.cnc_pdf_del(flib, ncPath);
+
+            FreeConnect(flib);
+
+            if (ret == 5) return "文件路径不正确或者文件没找到";
+            if (ret == 7) return "文件被保护";
+            if (ret == 13) return "CNC正在运行或者处于急停状态";
+            if (ret != 0) return GetGeneralErrorMessage(ret);
+            return null;
+        }
+
+    }
+}
