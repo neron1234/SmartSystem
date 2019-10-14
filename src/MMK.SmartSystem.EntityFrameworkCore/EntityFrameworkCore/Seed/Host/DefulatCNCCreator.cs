15ee6c63794d8de8ab8c60f4b2f68425068b89d3
Huang Yuan haung <dha834773759@163.com>
2019-10-14 13:05:10 +0800

    默认初始化数据

--- src/MMK.SmartSystem.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/DefulatCNCCreator.cs
+++ src/MMK.SmartSystem.EntityFrameworkCore/EntityFrameworkCore/Seed/Host/DefulatCNCCreator.cs
@@ -40,7 +40,57 @@ namespace MMK.SmartSystem.EntityFrameworkCore.Seed.Host
 
             #endregion
 
-            //_context.MachiningKinds.IgnoreQueryFilters().FirstOrDefault(d=>d.)
+            string[] kindTypes = { "", "大轮廓", "中轮廓", "小轮廓", "正常切割", "快速切割", "慢速切割", "打标", "其他", "其他", "其他" };
+
+            for (int i = 1; i <= 10; i++)
+            {
+                var kind = _context.MachiningKinds.IgnoreQueryFilters().FirstOrDefault(d => d.Code == i);
+                if (kind == null)
+                {
+                    _context.MachiningKinds.Add(new CNC.Core.LaserLibrary.MachiningKind()
+                    {
+                        Code = (short)i,
+                        Description = "初始化创建",
+                        Name_CN = kindTypes[i],
+                        Name_EN = kindTypes[i]
+                    });
+                }
+            }
+
+
+            string[] nozzleTypes = { "", "单层割嘴", "双层割嘴" };
+
+            for (int i = 1; i <= 2; i++)
+            {
+                var kind = _context.NozzleKinds.IgnoreQueryFilters().FirstOrDefault(d => d.Code == i);
+                if (kind == null)
+                {
+                    _context.NozzleKinds.Add(new CNC.Core.LaserLibrary.NozzleKind()
+                    {
+                        Code = (short)i,
+                        Name_CN = nozzleTypes[i],
+                        Name_EN = nozzleTypes[i]
+                    });
+                }
+            }
+
+
+            string[] matrialTypes = { "", "碳钢", "不锈钢", "贴膜不锈钢", "铝", "黄铜", "紫铜", "镀锌板", "覆铝锌", "自定义", "其他" };
+
+            for (int i = 1; i <= 10; i++)
+            {
+                var kind = _context.Materials.IgnoreQueryFilters().FirstOrDefault(d => d.Code == i);
+                if (kind == null)
+                {
+                    _context.Materials.Add(new CNC.Core.LaserLibrary.Material()
+                    {
+                        Code = (short)i,
+                        Name_CN = matrialTypes[i],
+                        Name_EN = matrialTypes[i],
+                        Description = "初始化创建"
+                    });
+                }
+            }
         }
     }
 }
