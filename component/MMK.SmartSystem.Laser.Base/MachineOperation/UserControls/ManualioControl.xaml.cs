23a6d95ca2614cd57fcdecddd9874cac1288a2ff
jinji.wu <aoe123jiji@163.com>
2019-10-25 17:32:25 +0800

    手动

--- component/MMK.SmartSystem.Laser.Base/MachineOperation/UserControls/ManualioControl.xaml.cs
+++ component/MMK.SmartSystem.Laser.Base/MachineOperation/UserControls/ManualioControl.xaml.cs
@@ -1,5 +1,4 @@
-﻿using Abp.Dependency;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,14 +13,14 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace MMK.SmartSystem.Laser.Base.MachineOperation
+namespace MMK.SmartSystem.Laser.Base.MachineOperation.UserControls
 {
     /// <summary>
-    /// MainMachineOperationPage.xaml 的交互逻辑
+    /// ManualioControl.xaml 的交互逻辑
     /// </summary>
-    public partial class MainMachineOperationPage : Page, ITransientDependency
+    public partial class ManualioControl : UserControl
     {
-        public MainMachineOperationPage()
+        public ManualioControl()
         {
             InitializeComponent();
         }
