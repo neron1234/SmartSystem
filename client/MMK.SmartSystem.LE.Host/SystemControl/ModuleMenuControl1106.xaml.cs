3dbadb7b2a6954569a4b4740fc268c99ca304398
jinji.wu <aoe123jiji@163.com>
2019-11-18 11:19:27 +0800

    CNC

---
+++ client/MMK.SmartSystem.LE.Host/SystemControl/ModuleMenuControl1106.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Abp.Dependency;
+using GalaSoft.MvvmLight.Messaging;
+using MMK.SmartSystem.LE.Host.SystemControl.ViewModel;
+using MMK.SmartSystem.LE.Host.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace MMK.SmartSystem.LE.Host.SystemControl
+{
+    /// <summary>
+    /// ModuleMenuControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class ModuleMenuControl1106 : UserControl
+    {
+        public ModuleMenuControl1106()
+        {
+            InitializeComponent();
+            //this.menuItem.ItemsSource= SmartSystemLEConsts.SystemModules;
+        }
+    }
+}
