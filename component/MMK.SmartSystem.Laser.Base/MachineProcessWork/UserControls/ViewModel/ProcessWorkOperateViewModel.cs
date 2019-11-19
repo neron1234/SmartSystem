1a37a3bbc055c82348c5636e0e92660b75448c88
jinji.wu <aoe123jiji@163.com>
2019-11-19 16:50:20 +0800

    ProcessWork

--- component/MMK.SmartSystem.Laser.Base/MachineProcessWork/UserControls/ViewModel/ProcessWorkOperateViewModel.cs
+++ component/MMK.SmartSystem.Laser.Base/MachineProcessWork/UserControls/ViewModel/ProcessWorkOperateViewModel.cs
@@ -26,62 +26,12 @@ namespace MMK.SmartSystem.Laser.Base.MachineProcessWork.UserControls.ViewModel
             }
         }
 
-        private System.Windows.Visibility _OperateVisibility;
-        public System.Windows.Visibility OperateVisibility
-        {
-            get { return _OperateVisibility; }
-            set
-            {
-                if (_OperateVisibility != value)
-                {
-                    _OperateVisibility = value;
-                    if (value == System.Windows.Visibility.Visible)
-                    {
-                        this.ProgramVisibility = System.Windows.Visibility.Collapsed;
-                    }
-                    RaisePropertyChanged(() => OperateVisibility);
-                }
-            }
-        }
-
-        private System.Windows.Visibility _ProgramVisibility;
-        public System.Windows.Visibility ProgramVisibility
-        {
-            get { return _ProgramVisibility; }
-            set
-            {
-                if (_ProgramVisibility != value)
-                {
-                    _ProgramVisibility = value;
-                    if (value == System.Windows.Visibility.Visible)
-                    {
-                        this.OperateVisibility = System.Windows.Visibility.Collapsed;
-                    }
-                    RaisePropertyChanged(() => ProgramVisibility);
-                }
-            }
-        }
-
-        public ICommand SwitchPageCommand{
-            get{
-                return new RelayCommand<string>((str) => {
-                    if (str == "0"){
-                        ProgramVisibility = System.Windows.Visibility.Visible;
-                    }else{
-                        OperateVisibility = System.Windows.Visibility.Visible;
-                    }
-                });
-            }
-        }
-
         public SwitchBtnViewModel BreakPointCutSwitchBtn { get; set; } = new SwitchBtnViewModel();
         public SwitchBtnViewModel NCutSwitchBtn { get; set; } = new SwitchBtnViewModel();
         public SwitchBtnViewModel StopSwitchBtn { get; set; } = new SwitchBtnViewModel();
 
         public ProcessWorkOperateViewModel()
         {
-            OperateVisibility = System.Windows.Visibility.Visible;
-            ProgramVisibility = System.Windows.Visibility.Collapsed;
             PreviewText = "(Model: LFK LCM3015) \r\n(Date: 10 / 15 / 19 Time: 11:16:36) \r\n(RST 37 - 2 0.8) \r\n(Sheet size: 3000x1500) \r\nG92 X0.0Y0.0 \r\nG0G90X38.57Y252.28 \r\nG13  \r\n G32 L1 E002  \r\n M70";
         }
     }
